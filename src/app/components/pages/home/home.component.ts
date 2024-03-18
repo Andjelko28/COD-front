83820e22f94e2e66eaff873f369ba742657491db
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-03-18 16:04:35 +0100

    Update home component with video embed and character images

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  safeSrc: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer){
+    this.safeSrc = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/v=bH1lHCirCGI');
+  }
 }
