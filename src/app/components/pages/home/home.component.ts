02a7efd1f42dce96718b002504eb23f63e186281
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-03-20 14:11:31 +0100

    Update CSS for trailer iframe and add click handler

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,Renderer2 } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class HomeComponent {
   safeSrc: SafeResourceUrl;
-  constructor(private sanitizer: DomSanitizer) { }
+  constructor(private sanitizer: DomSanitizer, private elementRef: ElementRef, private renderer:Renderer2) { }
 
   showTrailer() {
     this.safeSrc = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/bH1lHCirCGI');
