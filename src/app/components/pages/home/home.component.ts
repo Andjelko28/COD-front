0e04e90e9933dce51b7e26d9622d7cb80f39c3d3
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-03-25 16:21:14 +0100

    Remove ngx-bootstrap dependency and add carousel functionality to home component
    * Removed ngx-bootstrap package from package.json and package-lock.json
    * Added carousel component to home component with previous and next buttons
    * Added slides array and i variable to home component to keep track of current slide
    * Updated home component template to include carousel and buttons
    * Updated home component constructor to initialize slides array and i variable
    * Added getPrev() and getNext() functions to handle carousel navigation
    * Updated showVideoOverlay() function to toggle video overlay visibility
    * Commented out bootstrap import in src/styles.css as it is no longer needed

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -8,10 +8,26 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  slides: string[];
+  i: number;
   safeSrc: SafeResourceUrl;
   isVideoOverlayVisible: boolean = false;
   constructor(private sanitizer: DomSanitizer, private elementRef: ElementRef, private renderer: Renderer2) {
     this.safeSrc = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/ztjfwecrY8E');
+    this.i = 0;
+    this.slides = [
+      './assets/img/gameplay1.jpeg',
+      './assets/img/gameplay2.png'
+    ]
+  }
+
+
+  getPrev() {
+    this.i = this.i === 0 ? this.slides.length - 1 : this.i - 1;
+  }
+  
+  getNext() {
+    this.i = (this.i + 1) % this.slides.length;
   }
 
   showVideoOverlay() {
