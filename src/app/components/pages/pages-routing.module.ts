ba405104256eedb0e96fbe8b8cbb701f3293ef12
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-04-03 21:35:05 +0200

    "Add routerLink to MEDIA navbar item and create MediaComponent with route"
    This commit adds a routerLink to the MEDIA navbar item, allowing the user to navigate to a new page when clicked. It also creates a new component, MediaComponent, and adds a route for it in the pages-routing.module.ts file.

--- src/app/components/pages/pages-routing.module.ts
+++ src/app/components/pages/pages-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { TskComponent } from './tsk/tsk.component';
+import { MediaComponent } from './media/media.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
     path: 'task-force', component: TskComponent
+  },
+  {
+    path: 'media', component: MediaComponent
   }
 ];
 
