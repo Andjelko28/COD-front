3dfbc96c31a50ea343ce2ba12ce09e356d8f7a76
Andjelko28 <105885932+Andjelko28@users.noreply.github.com>
2024-04-03 21:33:06 +0200

    "Update header link to Task Force page and add Task Force component with routing"
    This commit includes changes to the header component's HTML to update the link for Task 141 to point to the new Task Force page, as well as the addition of a new component for the Task Force page and corresponding routing in the pages-routing module. The carousel has been removed from the home component's HTML.

---
+++ src/app/components/pages/tsk/tsk.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tsk',
+  standalone: true,
+  imports: [],
+  templateUrl: './tsk.component.html',
+  styleUrl: './tsk.component.css'
+})
+export class TskComponent {
+
+}
